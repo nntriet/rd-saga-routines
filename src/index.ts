c34d3811d81db308ba7a159e1a396e03d219921e
Nguyen Nhat Triet <triet@corsivalab.space>
2022-09-12 15:21:39 +0700

    fix: export createImmerReducer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type { Routine } from './createRoutine';
 export { createRoutine, createPlainAction as createAction } from './createRoutine';
-export { createReducer } from './createReducer';
+export { createReducer, createImmerReducer } from './createReducer';
 export { getType } from './getType';
 export { default as promisifyRoutine } from './promisifyRoutine';
 export { default as watchRoutinePromise } from './watchRoutinePromiseSaga';
